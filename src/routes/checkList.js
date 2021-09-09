7499fb5dabe21b5cd8cf8a27a783d3bdf4d8fa18
DreyIMM <andreydario26@gmail.com>
2021-09-09 18:58:28 -0300

    Create Routes, Mongoose and testing with postman

--- src/routes/checkList.js
+++ src/routes/checkList.js
@@ -1,11 +1,16 @@
 const Express = require ('express');
+
 const Router = Express.Router();
 const Checklist = require('../models/checklist')
 
 
-Router.get('/' , (req,res) =>{
-    console.log("Ola")
-    res.send()
+Router.get('/' , async (req,res) =>{
+    try{
+        let checklist = await Checklist.find({});
+        res.status(200).json(checklist)
+    }catch(error){
+        res.status(422).json(error)
+    }
 })
 
 Router.post('/', async (req,res) =>{
@@ -21,20 +26,34 @@ Router.post('/', async (req,res) =>{
     res.status(200).json(req.body)
 })
 
-Router.get('/:id' , (req,res) =>{
-    console.log(req.body["name"])
-    res.send(`ID: ${req.params.id}`)
+Router.get('/:id' , async (req,res) =>{
+    try{
+        let checklist = await Checklist.findById(req.params.id);
+        res.status(200).json(checklist);
+    }catch (error){
+        res.status(422).json(error)
+    }
 })
 
 
-Router.put('/:id', (req, res) =>{
-    console.log(req.params.id)
-    res.send(`PUT ID: ${req.params.id}`)
+Router.put('/:id', async(req, res) =>{
+    let {name} = req.body
+    try {
+        let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}, {new: true});
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)       
+    }
 })
 
-Router.delete('/:id' , (req,res) =>{
-    console.log(req.params.id)
-    res.send(`DELETE ID: ${req.params.id}`)
+Router.delete('/:id' , async (req,res) =>{
+    try {
+        let checklist = await Checklist.findByIdAndRemove(req.params.id);
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)  
+    }
 })
+
 
 module.exports = Router
