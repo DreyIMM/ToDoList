b1e1d5ab41a487b90bf2c14d62c23b2ba092a0d8
DreyIMM <andreydario26@gmail.com>
2021-09-22 20:55:28 -0300

    Integration with mongose

--- src/routes/checkList.js
+++ src/routes/checkList.js
@@ -1,57 +1,67 @@
-const Express = require ('express');
+const Express = require('express');
+const checklist = require('../models/checklist');
 
 const Router = Express.Router();
 const Checklist = require('../models/checklist')
 
 
-Router.get('/' , async (req,res) =>{
-    try{
-        let checklist = await Checklist.find({});
-        res.status(200).json(checklist)
-    }catch(error){
-        res.status(422).json(error)
-    }
-})
-
-Router.post('/', async (req,res) =>{
-    let {name} = req.body;
-    try{
-        let checklist = await Checklist.create({name})
-        res.status(200).json(checklist)
-    }catch(error){
-        res.status(422).json(error)
-    }
-
-
-    res.status(200).json(req.body)
-})
-
-Router.get('/:id' , async (req,res) =>{
-    try{
-        let checklist = await Checklist.findById(req.params.id);
-        res.status(200).json(checklist);
-    }catch (error){
-        res.status(422).json(error)
-    }
-})
-
-
-Router.put('/:id', async(req, res) =>{
-    let {name} = req.body
+Router.get('/', async (req, res) => {
     try {
-        let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}, {new: true});
+        let checklists = await Checklist.find({});
+        res.status(200).render('checklists/index', { checklists: checklists })
+    } catch (error) {
+        res.status(200).render('pages/error', { error: 'Erro ao exibir as listas' });
+    }
+})
+
+Router.get('/new', async (req, res) => {
+    try {
+        let checklist = new Checklist();
+        res.status(200).render('checklists/new', { checklist: checklist });
+    } catch (error) {
+        res.status(500).render('pages/error', { error: 'Erro ao carregar o formulario' })
+    }
+})
+
+Router.post('/', async (req, res) => {
+    let { name } = req.body.checklist;
+    let checklist = new Checklist({ name })
+
+    try {
+        await checklist.save();
+        res.redirect('/checkList');
+    } catch (error) {
+        res.status(422).render('checklists/new', { checklist: { ...checklist, error } })
+    }
+})
+
+Router.get('/:id', async (req, res) => {
+    try {
+        let checklist = await Checklist.findById(req.params.id);
+        res.status(200).render('checklists/show', { checklist: checklist })
+    } catch (error) {
+        res.status(500).render('pages/error', { error: 'Erro ao exibir as listas' });
+    }
+})
+
+
+Router.put('/:id', async (req, res) => {
+    let { name } = req.body
+    // é o campo que desejo atualizar 
+    try {
+        let checklist = await Checklist.findByIdAndUpdate(req.params.id, { name }, { new: true });
         res.status(200).json(checklist)
     } catch (error) {
-        res.status(422).json(error)       
+        res.status(422).json(error)
     }
 })
 
-Router.delete('/:id' , async (req,res) =>{
+Router.delete('/:id', async (req, res) => {
     try {
         let checklist = await Checklist.findByIdAndRemove(req.params.id);
         res.status(200).json(checklist)
     } catch (error) {
-        res.status(422).json(error)  
+        res.status(422).json(error)
     }
 })
 
